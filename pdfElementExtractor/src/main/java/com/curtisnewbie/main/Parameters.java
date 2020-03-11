35adb5ad117c26d139e976e8cd0424dfa9aaf2d9
CurtisNewbie <zhuangyongj@gmail.com>
2020-03-11 18:50:35 +0000

    Impl Parameters class that holds file paths

---
+++ pdfElementExtractor/src/main/java/com/curtisnewbie/main/Parameters.java
@@ -0,0 +1,20 @@
+package com.curtisnewbie.main;
+
+public class Parameters {
+
+    private String from;
+    private String to;
+
+    Parameters(String from, String to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public String getFrom() {
+        return this.from;
+    }
+
+    public String getTo() {
+        return this.to;
+    }
+}
